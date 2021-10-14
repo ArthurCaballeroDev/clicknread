726eba62efa9fff60fc4a42a126b5b1c830a6510
ArthurCaballeroDev <70220125+ArthurCaballeroDev@users.noreply.github.com>
2021-10-14 13:57:39 +0200

    Add forgotpassword.php

---
+++ forgotpassword.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Page d'inscription</title>
+    <link rel="stylesheet" href="/admin/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato&amp;display=swap">
+    <link rel="stylesheet" href="/admin/fonts/fontawesome-all.css">
+    <link rel="stylesheet" href="/admin/fonts/font-awesome.css">
+    <link rel="stylesheet" href="/admin/fonts/material-icons.css">
+    <link rel="stylesheet" href="/admin/fonts/fontawesome5-overrides.css">
+</head>
+	<body>
+		<div class='container'>
+        	<div class='card shadow-lg o-hidden border-0 my-5'>
+            	<div class='card-body p-0'>
+                	<div class='row'>
+                        <div class='p-5'>
+                            <div class='text-center'>
+									<h3>Vous ne vous souvenez plus de votre mot de passe ? </h3> 
+								<h4> Pas ne panique, entrez votre email ci-dessous afin de le réinitialiser </h4>
+								</br>
+							<form class="form-control" action="" method="post">
+                                <div class="mb-3">
+							<input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Adresse e-mail" name="email" required />
+						</div>
+							<button class="btn btn-primary d-block btn-user w-100" type="submit" style="background: rgb(33,37,41);">Réinitialiser mon mot de passe</button>
+							<a href="index.php"onclick="history.go(-1)">Retour</a>
+								</form>
+                            </div>
+						</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="/admin/js/script.js"></script>
+    <script src="/admin/js/theme.js"></script>
+</body>
+</html>
